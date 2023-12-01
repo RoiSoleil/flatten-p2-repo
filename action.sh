174ae7df25cb32f85077526e1ddf649a4b7a2c7e
Hélios GILLES <helios.gilles@gmail.com>
2023-12-01 19:02:31 +0100

    Test

--- action.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-export
-cd $GITHUB_ACTION_PATH
-wget https://repo1.maven.org/maven2/commons-io/commons-io/2.15.1/commons-io-2.15.1.jar
-wget https://repo1.maven.org/maven2/org/tukaani/xz/1.9/xz-1.9.jar
-export CLASSPATH=commons-io-2.15.1.jar\;xz-1.9.jar
-java FlattenP2Repo.java
