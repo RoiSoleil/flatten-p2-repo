b2f45c0d5e19ac0b1ca3feda4b31091cf1b0fd80
Hélios GILLES <helios.gilles@gmail.com>
2023-12-01 17:01:37 +0100

    Update action.yml

--- action.sh
+++ action.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
-cd ${{ github.action_path }}
+export
+cd $GITHUB_ACTION_PATH
 wget https://repo1.maven.org/maven2/commons-io/commons-io/2.15.1/commons-io-2.15.1.jar
 wget https://repo1.maven.org/maven2/org/tukaani/xz/1.9/xz-1.9.jar
 export CLASSPATH=commons-io-2.15.1.jar\;xz-1.9.jar
-java FlattenP2Repo.java ${{ inputs.database_url }}
+java FlattenP2Repo.java
